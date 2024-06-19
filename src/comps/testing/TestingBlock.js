5af6d80fc20f95e41e4d6534d749720b218c35d0
KarenAvetisyan <73932421+KarenAvetisyan@users.noreply.github.com>
2024-06-20 00:50:31 +0400

    Add files via upload

--- src/comps/testing/TestingBlock.js
+++ src/comps/testing/TestingBlock.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import { useState } from 'react'
 import './testing.css'
+import Image from '../image/Image'
+import icon__shop from '../../assets/images/shop.png'
+import icon__automobile from '../../assets/images/automobile.png'
+import icon__salon from '../../assets/images/salon.png'
+import icon__jim from '../../assets/images/jim.png'
+import icon__nedvijimost from '../../assets/images/nedvijimost.png'
+import icon__bank from '../../assets/images/bank.png'
 
 export default function TestingBlock() {
   const [status, setStatus] = useState('Статус');
+  const [select, setSelect] = useState('Выберите роль ИИ');
+  const [selectedImage, setSelectedImage] = useState();
+  const options = [
+    {icon: icon__shop, optionText: 'Менеджер в сфере розничной торговли'},
+    {icon: icon__automobile, optionText: 'Менеджер в автомобильном диллерском центре'},
+    {icon: icon__salon, optionText: 'Менеджер в салоне красоты'},
+    {icon: icon__jim, optionText: 'Менеджер в фитнес клубе'},
+    {icon: icon__nedvijimost, optionText: 'Менеджер в агенстве недвижимости'},
+    {icon: icon__bank, optionText: 'Менеджер в банковской сфере'},
+  ]
+  const [selectWrapClassActive, setSelectWrapClassActive ] = useState(false)
+
+  const handleToggleSelect = () => {
+    setSelectWrapClassActive(!selectWrapClassActive)
+  }
 
   return (
     <section className='sc-testing'>
@@ -18,12 +40,28 @@ export default function TestingBlock() {
                     <form>
                         <div className='form__head'>
                             <span className='status'>{status}</span>
-                            <select defaultValue={'Выберите роль ИИ'}>
-                                <option disabled>Выберите роль ИИ</option>
-                                <option>роль 1</option>
-                                <option>роль 2</option>
-                                <option>роль 3</option>
-                            </select>
+                            <div className={`select_wrap ${selectWrapClassActive?'active':''}`}>
+                                <ul className="default_option">
+                                    <li onClick={()=>handleToggleSelect()}>
+                                        <div className="option pizza">
+                                            <div className="icon"><Image img__src={selectedImage} /></div>
+                                            <p>{select}</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                                <ul className="select_ul">
+                                    {options.map((o, index)=>{
+                                        return (
+                                        <li key={index} onClick={()=>{setSelect(o.optionText); setSelectedImage(o.icon); setSelectWrapClassActive(false)}}>
+                                            <div className="option">
+                                                <div className="icon"> <Image img__src={o.icon} /> </div>
+                                                <p>{o.optionText}</p>
+                                            </div>
+                                        </li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
                         </div>
                         <div className='form__body'>
 
